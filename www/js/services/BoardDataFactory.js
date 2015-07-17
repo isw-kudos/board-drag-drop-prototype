1e22b58eb2d2856c17924da71c42d91bce1c4929
andylwelch <awelch@isw.net.au>
2015-07-17 10:59:12 +1000

    Data - add 'commonType' & 'completed' attributes

--- www/js/services/BoardDataFactory.js
+++ www/js/services/BoardDataFactory.js
@@ -9,47 +9,52 @@ angular.module('boards').service('BoardDataFactory', function () {
     kanban: {
       "id":"0",
       "name": "Kanban Board",
+      "commonType":"board",
       "childNodes": [
         {
           "id":"1",
           "name": "Ideas",
+          "commonType":"section",
           "childNodes": [
-            {"id":"a","name": "Come up with a POC for new Project"},
-            {"id":"b","name": "Design new framework for reporting module"},
-            {"id":"c","name": "Design new work for reporting module"},
-            {"id":"d","name": "Design new UI for reporting module"},
-            {"id":"e","name": "Design new work for reporting module"},
-            {"id":"f","name": "Design new UI for reporting module"},
-            {"id":"g","name": "Design new work for reporting module"},
-            {"id":"h","name": "Design new UI for reporting module"},
-            {"id":"i","name": "Design new stub for reporting module"},
-            {"id":"j","name": "Design factory for reporting module"},
-            {"id":"k","name": "Resolve framework"}
+            {"id":"a","name": "Come up with a POC for new Project","commonType":"todo","completed":0},
+            {"id":"b","name": "Design new framework for reporting module","commonType":"entry"},
+            {"id":"c","name": "Design new work for reporting module","commonType":"entry"},
+            {"id":"d","name": "Design new UI for reporting module","commonType":"todo","completed":1},
+            {"id":"e","name": "Design new work for reporting module","commonType":"entry"},
+            {"id":"f","name": "Design new UI for reporting module","commonType":"todo","completed":1},
+            {"id":"g","name": "Design new work for reporting module","commonType":"todo","completed":0},
+            {"id":"h","name": "Design new UI for reporting module","commonType":"entry"},
+            {"id":"i","name": "Design new stub for reporting module","commonType":"todo","completed":0},
+            {"id":"j","name": "Design factory for reporting module","commonType":"todo","completed":1},
+            {"id":"k","name": "Resolve framework","commonType":"entry"}
           ]
         },
         {
           "id":"2",
           "name": "Not started",
+          "commonType":"section",
           "childNodes": [
-            {"id":"2a","name": "Explore new IDE for Development","description": "Testing Card Details"},
-            {"id":"2b","name": "Get new resource for new Project","description": "Testing Card Details"}
+            {"id":"2a","name": "Explore new IDE for Development","description": "Testing Card Details","commonType":"todo","completed":0},
+            {"id":"2b","name": "Get new resource for new Project","description": "Testing Card Details","commonType":"entry"}
           ]
         },
         {
           "id":"3",
           "name": "In progress",
+          "commonType":"section",
           "childNodes": [
-            {"id":"3a","name": "Develop ui for tracker module","description": "Testing Card Details"},
-            {"id":"3b","name": "Develop backend for plan module","description": "Testing Card Details"}
+            {"id":"3a","name": "Develop ui for tracker module","description": "Testing Card Details","commonType":"entry"},
+            {"id":"3b","name": "Develop backend for plan module","description": "Testing Card Details","commonType":"entry"}
           ]
         },
         {
           "id":"4",
           "name": "Done",
+          "commonType":"section",
           "childNodes": [
-            {"id":"4a","name": "Test user module","description": "Testing Card Details"},
-            {"id":"4b","name": "End to End Testing for user group module","description": "Testing Card Details"},
-            {"id":"4c","name": "CI for user module","description": "Testing Card Details"}
+            {"id":"4a","name": "Test user module","description": "Testing Card Details","commonType":"todo","completed":0},
+            {"id":"4b","name": "End to End Testing for user group module","description": "Testing Card Details","commonType":"entry"},
+            {"id":"4c","name": "CI for user module","description": "Testing Card Details","commonType":"todo","completed":0}
           ]
         }
       ]
